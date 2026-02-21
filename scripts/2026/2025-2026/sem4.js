733337487a3ae8614b1ee98efa36d057c9438f4a
Luminex Nova <dearharv@proton.me>
2026-02-21 13:11:27 +0530

    Add files via upload

---
+++ scripts/2026/2025-2026/sem4.js
@@ -0,0 +1,42 @@
+// File Path: scripts/2025-2026/sem4.js
+
+window.currentRoutineData = {
+  "MON": [
+    { start: "10:30", end: "11:30", subject: "IDC" },
+    { start: "11:30", end: "12:30", subject: "IDC" },
+    { start: "12:30", end: "01:30", subject: "MAJ8 - Dr. Md Hasanujjaman" },
+    { start: "02:00", end: "03:00", subject: "MAJ8 - Dr. Md Hasanujjaman" },
+    { start: "03:00", end: "04:00", subject: "VAC" },
+    { start: "04:00", end: "05:00", subject: "VAC" }
+  ],
+  "TUE": [
+    { start: "10:30", end: "11:30", subject: "MAJ7 - Dr. Koustuv Roy" },
+    { start: "11:30", end: "12:30", subject: "MIN4 - Mr. Ikbal Mandal" },
+    { start: "02:00", end: "03:00", subject: "MIN4 - Dr. Supriya Chatterjee" },
+    { start: "03:00", end: "04:00", subject: "MAJ8 - Dr. Md Hasanujjaman" }
+  ],
+  "WED": [
+    { start: "10:30", end: "11:30", subject: "MAJ7 - Dr. Koustuv Roy" },
+    { start: "02:00", end: "03:00", subject: "MIN4 - Mr. Ikbal Mandal" },
+    { start: "03:00", end: "04:00", subject: "MAJ7 Practical - Dr. Koustuv Roy" },
+    { start: "04:00", end: "05:00", subject: "MAJ7 Practical - Dr. Koustuv Roy" }
+  ],
+  "THU": [
+    { start: "10:30", end: "11:30", subject: "MAJ7 - Dr. Koustuv Roy" },
+    { start: "11:30", end: "12:30", subject: "MIN4 Practical - Dr. Supriya Chatterjee" },
+    { start: "02:00", end: "03:00", subject: "MIN4 Practical - Mr. Ikbal Mandal" },
+    { start: "03:00", end: "04:00", subject: "MAJ8 - Dr. Md Hasanujjaman" }
+  ],
+  "FRI": [
+    { start: "10:30", end: "11:30", subject: "IDC" },
+    { start: "11:30", end: "12:30", subject: "IDC" },
+    { start: "03:00", end: "04:00", subject: "VAC" },
+    { start: "04:00", end: "05:00", subject: "VAC" }
+  ],
+  "SAT": [
+    { start: "10:30", end: "11:30", subject: "Remedial Class - Dr. Koustuv Roy" },
+    { start: "11:30", end: "12:30", subject: "Remedial Class - Dr. Supriya Chatterjee" },
+    { start: "02:00", end: "03:00", subject: "Tutorial - Mr. Biju Saha" }
+  ],
+  "SUN": [] // Sunday te kono class nei, kintu empty array rakha bhalo jate error na ase
+};
