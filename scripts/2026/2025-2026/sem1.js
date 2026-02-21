bad95133547dd6696dfd850df8e0e6f728115070
Luminex Nova <dearharv@proton.me>
2026-02-21 11:27:53 +0530

    Add files via upload

---
+++ scripts/2026/2025-2026/sem1.js
@@ -0,0 +1,52 @@
+// File Path: scripts/2025-2026/sem1.js
+
+window.currentRoutineData = {
+  "MON": [
+    { start: "10:30", end: "11:30", subject: "MAJ2 - Dr. Md Hasanujjaman" },
+    { start: "11:30", end: "12:30", subject: "MAJ2 - Dr. Md Hasanujjaman" },
+    { start: "12:30", end: "01:30", subject: "VAC - Dr. Nivedita Chatterjee" },
+    { start: "02:00", end: "03:00", subject: "VAC - Dr. Nivedita Chatterjee" },
+    { start: "03:00", end: "04:00", subject: "SEC - Dr. Md Hasanujjaman" },
+    { start: "04:00", end: "05:00", subject: "SEC - Dr. Koustuv Roy" }
+  ],
+  "TUE": [
+    { start: "10:30", end: "11:30", subject: "MIN1 - Dr. Md Hasanujjaman" },
+    { start: "11:30", end: "12:30", subject: "MIN1 - Dr. Supriya Chatterjee" },
+    { start: "12:30", end: "01:30", subject: "MAJ2 Practical - Mr. Ikbal Mandal" },
+    { start: "02:00", end: "03:00", subject: "MAJ2 Practical - Mr. Ikbal Mandal" },
+    { start: "03:00", end: "04:00", subject: "MAJ1 Practical - Mr. Ikbal Mandal" },
+    { start: "04:00", end: "05:00", subject: "MAJ1 Practical - Mr. Ikbal Mandal & Dr. Md Hasanujjaman" }
+  ],
+  "WED": [
+    { start: "10:30", end: "11:30", subject: "MAJ1 - Mr. Ikbal Mandal" },
+    { start: "11:30", end: "12:30", subject: "MAJ1 - Mr. Ikbal Mandal" },
+    { start: "12:30", end: "01:30", subject: "MIN1 - Mr. Ikbal Mandal" },
+    { start: "01:30", end: "02:00", subject: "Recess Hour" },
+    { start: "02:00", end: "03:00", subject: "MAJ2 - Mr. Hirujit Ghosh" },
+    { start: "03:00", end: "04:00", subject: "No classes" },
+    { start: "04:00", end: "05:00", subject: "MAJ1 - Dr. Koustuv Roy" }
+  ],
+  "THU": [
+    { start: "10:30", end: "11:30", subject: "MIN1 Practical - Mr. Ikbal Mandal & Mr. Hirujit Ghosh" },
+    { start: "11:30", end: "12:30", subject: "MIN1 Practical - Mr. Ikbal Mandal & Mr. Hirujit Ghosh" },
+    { start: "12:30", end: "01:30", subject: "Recess Hour" },
+    { start: "02:00", end: "03:00", subject: "MAJ1 - Dr. Koustuv Roy" },
+    { start: "03:00", end: "04:00", subject: "MAJ2 - Dr. Supriya Chatterjee" },
+    { start: "04:00", end: "05:00", subject: "MAJ2 - Dr. Supriya Chatterjee" }
+  ],
+  "FRI": [
+    { start: "10:30", end: "11:30", subject: "MAJ1 - Mr. Ikbal Mandal" },
+    { start: "11:30", end: "12:30", subject: "MAJ2 - Mr. Hirujit Ghosh" },
+    { start: "12:30", end: "01:30", subject: "VAC - Dr. Nivedita Chatterjee" },
+    { start: "02:20", end: "03:00", subject: "VAC - Dr. Nivedita Chatterjee" },
+    { start: "03:00", end: "04:00", subject: "SEC Practical - Mr. Hirujit Ghosh & Dr. Supriya Chatterjee" },
+    { start: "04:00", end: "05:00", subject: "SEC Practical - Mr. Hirujit Ghosh & Dr. Supriya Chatterjee" }
+  ],
+  "SAT": [
+    { start: "10:30", end: "11:30", subject: "Remedial Class - Dr. Md Hasanujjaman" },
+    { start: "11:30", end: "12:30", subject: "Remedial Class - Mr. Ikbal Mandal" },
+    { start: "12:30", end: "01:30", subject: "Remedial Class - Mr. Hirujit Ghosh" },
+    { start: "02:00", end: "03:00", subject: "Tutorial - Ms. Nilanjana Nandi" }
+  ],
+  "SUN": [] // Sunday te kono class nei, kintu empty array rakha bhalo jate error na ase
+};
