78984b2156cc3d433173b18b1b2628d618ac381f
Luminex Nova <dearharv@proton.me>
2026-02-21 13:34:02 +0530

    Add files via upload

--- scripts/2026/2026holiday.js
+++ scripts/2026/2026holiday.js
@@ -4,30 +4,36 @@
   // 1. Single Day Holidays (Strictly Format: MM/DD/YYYY)
   const singleHolidays = [
     "01/01/2026", // New Year's Day
+    "01/12/2026", // Birthday of Swami Vivekananda
     "01/14/2026", // Makar Sankranti
-    "01/23/2026", // Birthday of Netaji
+    "01/22/2026", // Day before Saraswati Puja
+    "01/23/2026", // Birthday of Netaji and Saraswati Puja
     "01/26/2026", // Republic Day
-    "02/03/2026", // Saraswati Puja / Day After
+    "02/04/2026", // Shab-e-Barat
     "02/14/2026", // Birthday of Thakur Panchanan Barma
-    "02/26/2026", // Maha Shivaratri
-    "03/14/2026", // Doljatra & Holi
-    "03/15/2026", // Day after Doljatra
-    "03/27/2026", // Birthday of Shri Shri Harichand Thakur
-    "03/31/2026", // Eid-ul-Fitar
-    "04/01/2026", // Day after Eid-ul-Fitar
-    "04/10/2026", // Mahavir Jayanti
+    "02/15/2026", // Maha Shivaratri
+    "03/03/2026", // Doljatra
+    "03/04/2026", // Holi (Day after Doljatra)
+    "03/17/2026", // Birthday of Shri Shri Harichand Thakur
+    "03/20/2026", // Day before Eid-ul-Fitar
+    "03/21/2026", // Eid-ul-Fitar
+    "03/26/2026", // Ram Navami
+    "03/31/2026", // Mahavir Jayanti
+    "04/03/2026", // Good Friday
     "04/14/2026", // Birthday of Dr. B.R. Ambedkar
     "04/15/2026", // Bengali New Year's Day
-    "04/18/2026", // Good Friday
-    "05/01/2026", // May Day
-    "06/27/2026", // Rathayatra
+    "05/01/2026", // May Day and Buddha Purnima
+    "05/09/2026", // Birthday of Rabindranath Tagore
+    "07/13/2026", // Birthday of Poet Bhanu Bhakt
+    "07/16/2026", // Rathayatra
     "07/21/2026", // College Foundation Day
     "08/02/2026", // Birthday of Acharya Prafulla Chandra Roy
-    "08/09/2026", // Rakhi Bandhan
     "08/15/2026", // Independence Day
-    "09/05/2026", // Fateha-Dwaz-Daham
+    "08/26/2026", // Fateha-Dwaz-Daham
+    "08/28/2026", // Rakhi Bandhan
+    "09/04/2026", // Janmashtami
     "09/17/2026", // Viswakarma Puja
-    "12/31/2026"  // Principal's Discretion
+    "10/02/2026"  // Birthday of Mahatma Gandhi
   ];
 
   // Array to hold the final list
@@ -55,9 +61,9 @@
   }
 
   // 2. Add Vacation Ranges (Format: MM/DD/YYYY)
-  addVacationDays("05/05/2026", "06/27/2026"); // Summer Vacation
-  addVacationDays("09/22/2026", "11/05/2026"); // Puja Vacation
-  addVacationDays("12/25/2026", "12/31/2026"); // Winter Vacation
+  addVacationDays("05/16/2026", "06/30/2026"); // Summer Vacation
+  addVacationDays("10/10/2026", "11/24/2026"); // Puja Vacation
+  addVacationDays("12/25/2026", "12/31/2026"); // Winter Recess
 
   // 3. Assign to global window object so main HTML can read it
   window.holidaysList = finalHolidays;
