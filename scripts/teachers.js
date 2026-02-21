f2c5849a4c818028a721100b18a141eb2877b91d
Luminex Nova <dearharv@proton.me>
2026-02-21 11:24:25 +0530

    Add files via upload

---
+++ scripts/teachers.js
@@ -0,0 +1,11 @@
+// File Path: scripts/teachers.js
+
+window.teacherURLs = {
+  "Dr. Md Hasanujjaman": "http://www.apcrgc.org/dept-of-physics-our-departments.html",
+  "Dr. Koustuv Roy": "http://www.apcrgc.org/dept-of-physics-our-departments.html",
+  "Dr. Supriya Chatterjee": "http://www.apcrgc.org/dept-of-physics-our-departments.html",
+  "Mr. Ikbal Mandal": "http://www.apcrgc.org/dept-of-physics-our-departments.html",
+  "Mr. Hirujit Ghosh": "http://www.apcrgc.org/dept-of-physics-our-departments.html",
+  "Ms. Nilanjana Nandi": "http://www.apcrgc.org/dept-of-physics-our-departments.html",
+  "Dr. Nivedita Chatterjee": " " // Ekhane blank url ache, future e add kore nite parbe
+};
